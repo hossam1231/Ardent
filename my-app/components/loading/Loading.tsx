1708aa3ad13e12a8d0981617462dcd1a4bb60816
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-02-24 12:55:26 +0000

    push

---
+++ my-app/components/loading/Loading.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { MotiView, AnimatePresence } from "moti";
+import { StyleSheet, Pressable } from "react-native";
+
+export const LoadingIndicator = ({ size }: { size: number }) => {
+  return (
+    <AnimatePresence>
+      <MotiView>
+        from=
+        {{
+          width: size,
+          height: size,
+          borderRadius: size / 2,
+        }}
+        animate=
+        {{
+          width: size + 20,
+          height: size + 20,
+          borderRadius: (size + 20) / 2,
+        }}
+        transition=
+        {{
+          type: "timing",
+          duration: 1000,
+          repeat: 3,
+        }}
+        style=
+        {{
+          width: size,
+          height: size,
+          borderRadius: size / 2,
+          borderWidth: size / 10,
+          borderColor: "#fff",
+          borderStyle: "solid",
+          shadowColor: "#fff",
+          shadowOffset: { width: 0, height: 0 },
+          shadowOpacity: 1,
+          shadowRadius: 10,
+        }}
+      </MotiView>
+    </AnimatePresence>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   shape: {
+//     justifyContent: "center",
+//     height: 250,
+//     width: 250,
+//     borderRadius: 25,
+//     marginRight: 10,
+//     backgroundColor: "white",
+//   },
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     flexDirection: "row",
+//     backgroundColor: "#9c1aff",
+//   },
+// });
+
+export default LoadingIndicator;

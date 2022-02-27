576d6e8aa7afc10d25860bf7a6059b8efd663ed9
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-02-27 00:25:00 +0000

    fonts added

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { NativeBaseProvider } from "native-base";
-import { StyleSheet, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
 import Product from "./components/product/Product";
+import Loading from "./components/loading/Loading";
+
+import { useFonts } from "expo-font";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    FrancoisOne: require("./assets/fonts/FrancoisOne/FrancoisOne.ttf"),
+    OpenSans: require("./assets/fonts/OpenSans/OpenSans.ttf"),
+    OpenSansBold: require("./assets/fonts/OpenSans/OpenSansBold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <NativeBaseProvider>
+        <Loading size={100} />
+      </NativeBaseProvider>
+    );
+  }
+
   return (
     <NativeBaseProvider>
       <Product />
--- my-app/components/product/Product.jsx
+++ my-app/components/product/Product.jsx
@@ -1,52 +1,108 @@
-import React from "react";
-import { Center, Box, HStack, Text } from "native-base";
+import React, { useCallback, useMemo, useRef } from "react";
+import {
+  Center,
+  Box,
+  HStack,
+  Text,
+  IconButton,
+  Badge,
+  Icon,
+  VStack,
+} from "native-base";
 import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import {
+  Entypo,
+  AntDesign,
+  SimpleLineIcons,
+  Ionicons,
+} from "@expo/vector-icons";
 
 export const Product = () => {
+  // ref
+  const bottomSheetRef = useRef(BottomSheet);
+
+  // variables
+  const snapPoints = useMemo(() => ["50%", "25%"], []);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
+  // renders
   return (
     <Box p="20px" flex="1">
       <HStack
         mt="20px"
-        mb="10px"
         h="50px"
         w="100%"
-        bg="black"
         alignItems="center"
         justifyContent="space-between"
       >
         {/* TOP BAR  */}
-        <Box w="10px" h="10px">
+        <Box justifyContent="center" w="50px" h="50px">
           {/* close icon  */}
+          <IconButton
+            icon={<Icon as={AntDesign} name="close" />}
+            borderRadius="full"
+          />
         </Box>
-        <Box w="10px" h="10px">
+
+        <Box justifyContent="center" w="50px" h="50px">
           {/* share icon  */}
+          <IconButton
+            icon={<Icon as={Ionicons} name="share-outline" />}
+            borderRadius="full"
+          />
         </Box>
       </HStack>
 
       <Box flex="1" bg="black" p="2px">
         {/* PRODUCT IMAGE */}
       </Box>
-
-      <Box>
-        {/* BOTTOMCARD */}
-        <HStack h="40px" alignItems="center" justifyContent="space-between">
-          <Center h="20px" w="40px" bg="black">
-            {/* Add to basket  */}
-            <Text>ADD TO BAG</Text>
-          </Center>
-
-          <Box>
-            <HStack justifyContent="space-between">
-              <Box h="10px" w="10px">
-                {/* bookmark icon */}
-              </Box>
-              <Box h="10px" w="10px">
-                {/* basket bookmark icon */}
-              </Box>
-            </HStack>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+      >
+        <Box mt="5px" p="20px" flex="1">
+          {/* BOTTOMCARD */}
+          <Box mb="10px">
+            <Text fontFamily="OpenSans">NEW</Text>
+            <Text fontFamily="FrancoisOne">TEXTURED PINAFORE SKIRT </Text>
+            <Text fontFamily="OpenSans">$29.99</Text>
           </Box>
-        </HStack>
-      </Box>
+
+          <HStack h="40px" alignItems="center" justifyContent="space-between">
+            <Center h="40px" w="120px" bg="black">
+              {/* Add to basket  */}
+              <Text fontFamily="OpenSansBold" color="white">
+                ADD TO BAG
+              </Text>
+            </Center>
+
+            <Box>
+              <HStack justifyContent="space-between">
+                <Box w="50px" h="50px">
+                  {/* bookmark icon */}
+                  <IconButton
+                    icon={<Icon as={Ionicons} name="bookmark-outline" />}
+                    borderRadius="full"
+                  />
+                </Box>
+                <Box w="50px" h="50px">
+                  {/* bag icon */}
+                  <IconButton
+                    icon={<Icon as={SimpleLineIcons} name="bag" />}
+                    borderRadius="full"
+                  />
+                </Box>
+              </HStack>
+            </Box>
+          </HStack>
+        </Box>
+      </BottomSheet>
     </Box>
   );
 };
